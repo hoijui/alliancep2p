8afd8227b35cabb1a9fa0988ef4e9bbbe59fc0de
maciekdrejak <maciekdrejak@b55371b8-5f14-0410-a052-90a574d2d31b>
2006-08-14 07:25:07 +0000

    fixed bug in share browsing

--- src/org/alliance/Version.java
+++ src/org/alliance/Version.java
@@ -12,7 +12,7 @@ import java.util.Properties;
  * To change this template use File | Settings | File Templates.
  */
 public class Version {
-    public static final String VERSION="0.8.30 BETA";
+    public static final String VERSION="0.8.30";
     public static final int BUILD_NUMBER;
     public static final int PROTOCOL_VERSION = 2;
 
