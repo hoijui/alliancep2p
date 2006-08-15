2c56346a8bf362171bdabd8858a51790c462835c
mrund <mrund@b55371b8-5f14-0410-a052-90a574d2d31b>
2006-08-15 09:46:16 +0000

    2nd degree connnections list is now sorted by nickname the list is also expanded to 200 pixels if possible (= 1 extra nickname is visiable)

--- src/org/alliance/ui/addfriendwizard/ForwardInvitationNodesList.java
+++ src/org/alliance/ui/addfriendwizard/ForwardInvitationNodesList.java
@@ -9,7 +9,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.HashSet;
+import java.util.Comparator;
+import java.util.TreeSet;
+import java.util.Collections;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,7 +32,7 @@ public class ForwardInvitationNodesList extends JList {
         addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 int index = locationToIndex(e.getPoint());
-                ListRow item = (ListRow)getModel().getElementAt(index);
+                ListRow item = (ListRow) getModel().getElementAt(index);
                 item.selected = !item.selected;
                 if (item.selected) addFriendWizard.enableNext();
                 Rectangle rect = getCellBounds(index, index);
@@ -42,8 +44,8 @@ public class ForwardInvitationNodesList extends JList {
     }
 
     public void selectAll() {
-        for(int i=0;i<getModel().getSize();i++) {
-            ((ListRow)getModel().getElementAt(i)).selected = true;
+        for (int i = 0; i < getModel().getSize(); i++) {
+            ((ListRow) getModel().getElementAt(i)).selected = true;
             addFriendWizard.enableNext();
         }
 
@@ -59,7 +61,7 @@ public class ForwardInvitationNodesList extends JList {
         public Component getListCellRendererComponent(JList list, Object value,
                                                       int index, boolean isSelected, boolean hasFocus) {
             setEnabled(list.isEnabled());
-            setSelected(((ListRow)value).selected);
+            setSelected(((ListRow) value).selected);
             setFont(list.getFont());
             setText(value.toString());
             return this;
@@ -77,7 +79,7 @@ public class ForwardInvitationNodesList extends JList {
             this.connectedThrough = connectedThrough;
             this.guid = guid;
 
-            toString = "<html>"+nickname+" <font color=gray>(connected to "+connectedThrough+")</font></html>";
+            toString = "<html>" + nickname + " <font color=gray>(connected to " + connectedThrough + ")</font></html>";
         }
 
         public String toString() {
@@ -88,30 +90,40 @@ public class ForwardInvitationNodesList extends JList {
     private static class ForwardInvitationListModel extends DefaultListModel {
         private UISubsystem ui;
 
-        public ForwardInvitationListModel(UISubsystem ui) {
+        public ForwardInvitationListModel(final UISubsystem ui) {
             this.ui = ui;
 
-            HashSet<Integer> secondaryNodeGuids = new HashSet<Integer>();
-            for(Friend f : ui.getCore().getFriendManager().friends()) {
+            TreeSet<Integer> secondaryNodeGuids = new TreeSet<Integer>(new Comparator<Integer>() {
+                public int compare(Integer o1, Integer o2) {
+                    String n1 = ui.getCore().getFriendManager().nickname(o1);
+                    String n2 = ui.getCore().getFriendManager().nickname(o2);
+                    if (n1.compareToIgnoreCase(n2) == 0) {
+                        return o1.compareTo(o2);
+                    } else {
+                        return n1.compareToIgnoreCase(n2);
+                    }
+                }
+            });
+            for (Friend f : ui.getCore().getFriendManager().friends()) {
                 if (f.friendsFriends() != null) {
-                    for(UntrustedNode n: f.friendsFriends()) {
+                    for (UntrustedNode n : f.friendsFriends()) {
                         if (ui.getCore().getFriendManager().getFriend(n.getGuid()) == null && ui.getCore().getFriendManager().getMyGUID() != n.getGuid())
                             secondaryNodeGuids.add(n.getGuid());
                     }
                 }
             }
-
-            for(int guid : secondaryNodeGuids) {
+            for (int guid : secondaryNodeGuids) {
                 if (!ui.getCore().getInvitaitonManager().hasBeenRecentlyInvited(guid))
                     addElement(new ListRow(ui.getCore().getFriendManager().nickname(guid), createConnectedThroughList(guid), guid));
             }
 
-            if (getSize() == 0) OptionDialog.showInformationDialog(ui.getMainWindow(), "No new connections where found for you to connect to.");
+            if (getSize() == 0)
+                OptionDialog.showInformationDialog(ui.getMainWindow(), "No new connections where found for you to connect to.");
         }
 
         private String createConnectedThroughList(int guid) {
             String s = "";
-            for(Friend f : ui.getCore().getFriendManager().friends()) {
+            for (Friend f : ui.getCore().getFriendManager().friends()) {
                 if (f.getFriendsFriend(guid) != null) {
                     if (s.length() > 0) s += ", ";
                     s += f.getNickname();
@@ -122,14 +134,14 @@ public class ForwardInvitationNodesList extends JList {
     }
 
     public void forwardSelectedInvitations() {
-        for(int i=0;i<getModel().getSize();i++) {
-            final ListRow r = (ListRow)getModel().getElementAt(i);
+        for (int i = 0; i < getModel().getSize(); i++) {
+            final ListRow r = (ListRow) getModel().getElementAt(i);
             if (r.selected) {
                 ui.getCore().invokeLater(new Runnable() {
                     public void run() {
                         try {
                             ui.getCore().getFriendManager().forwardInvitationTo(r.guid);
-                        } catch(Exception e) {
+                        } catch (Exception e) {
                             ui.getCore().reportError(e, this);
                         }
                     }
