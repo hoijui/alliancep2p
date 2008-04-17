8ed8361988244cef051ddadadc5f9799c24f6700
maciekdrejak <maciekdrejak@b55371b8-5f14-0410-a052-90a574d2d31b>
2008-04-17 13:24:47 +0000

    working on version 0.9.9

--- src/org/alliance/Version.java
+++ src/org/alliance/Version.java
@@ -12,7 +12,7 @@ import java.util.Properties;
  * To change this template use File | Settings | File Templates.
  */
 public class Version {
-    public static final String VERSION="0.9.9 indev";
+    public static final String VERSION="1.0.0 indev";
     public static final int BUILD_NUMBER;
     public static final int PROTOCOL_VERSION = 3;
 
