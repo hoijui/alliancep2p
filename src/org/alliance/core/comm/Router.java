7e94b81a4a4b9e25cb494ea7e1ebc33134b604e4
maciekdrejak <maciekdrejak@b55371b8-5f14-0410-a052-90a574d2d31b>
2008-12-10 13:38:56 +0000

    improved alliance router class scalability

--- src/org/alliance/core/comm/Router.java
+++ src/org/alliance/core/comm/Router.java
@@ -15,9 +15,10 @@ import java.util.HashMap;
  */
 public class Router {
     private class Route {
-        int friendGuid, hops;
+        int friendGuid;
+        byte hops;
 
-        public Route(int friendGuid, int hops) {
+        public Route(int friendGuid, byte hops) {
             this.friendGuid = friendGuid;
             this.hops = hops;
         }
@@ -31,27 +32,42 @@ public class Router {
     }
 
     public void updateRouteTable(Friend friend, int nodeGuid, int hops) {
+        if (manager.getFriend(nodeGuid) != null) return;
+        if (hops == 1) {
+            if(T.netTrace)T.trace("Number of hops is 1. Can find this user using the friend managers friends of friends. Don't add him to the route table - this saves a lot of memory");
+            return;
+        }
         Route routes[] = routeTable.get(nodeGuid);
+        System.out.println("Updating route table. Friend: "+friend+", remote is friend: "+(manager.getFriend(nodeGuid) != null)+" table size: "+routeTable.size()+", total size: "+totalSize());
         if (routes == null) {
             if(T.netTrace)T.trace("Adding new Route to "+nodeGuid+" via "+friend);
-            routeTable.put(nodeGuid, new Route[] {new Route(friend.getGuid(), hops)});
+            routeTable.put(nodeGuid, new Route[] {new Route(friend.getGuid(), (byte)hops)});
         } else {
             if(T.netTrace)T.trace("Updating existing Route array (Route to "+nodeGuid+" via "+friend+")");
             for(Route r : routes) {
                 if (r.friendGuid == friend.getGuid()) {
                     if (r.hops > hops) {
                         if(T.netTrace)T.trace("Updating number of hops for existing Route to"+nodeGuid+". Old: "+r.hops+" new: "+hops);
-                        r.hops = hops;
+                        r.hops = (byte)hops;
                         return;
                     }
                 }
             }
+
             if(T.netTrace)T.trace("Adding new Route");
             Route r[] = new Route[routes.length+1];
-            r[0] = new Route(friend.getGuid(), hops);
+            r[0] = new Route(friend.getGuid(), (byte)hops);
             System.arraycopy(routes, 0, r, 1, routes.length);
             routeTable.put(nodeGuid, r);
         }
+    }
+
+    private int totalSize() {
+        int n=0;
+        for(Route[] routes : routeTable.values()) {
+            n+=routes.length;
+        }
+        return n;
     }
 
     public Friend findClosestFriend(int nodeGuid) {
@@ -63,6 +79,13 @@ public class Router {
             return f;
         }
 
+        if (manager.getUntrustedNode(nodeGuid) != null) {
+            //a friend of mine should have this node as a friend. Let's try to find him this way - this is why we don't have to save routes that are one hop away
+            for(Friend friend : manager.friends()) {
+                if (friend.getFriendsFriends().get(nodeGuid) != null) return friend;
+            }
+        }
+        
         Route routes[] = routeTable.get(nodeGuid);
         if (routes == null) return null;
         int minHops=0, index=-1;
